e96b6925d24899def109db901596a2f9fb526d8b
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-04-01 19:26:47 +0300

    add renders button correctly test

--- src/components/button/__test__/button.test.js
+++ src/components/button/__test__/button.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './../button';
+import { render } from '@testing-library/react';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Button></Button>, div);
+    ReactDOM.render(<Button />, div);
+});
+
+it('renders button correctly', () => {
+    const { getByTestId } = render(
+        <Button label={'a very important button'}></Button>
+    );
+    expect(getByTestId('button')).toHaveTextContent('a very important button');
 });
