f250e29f22f9419d041c82a9d282398f62e6d6ac
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-04-01 18:54:39 +0300

    add btn component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Button from './components/button';
 
 function App() {
-    return <div className='App'>Test</div>;
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <Button label={'a very important button'} />
+            </header>
+        </div>
+    );
 }
 
 export default App;
